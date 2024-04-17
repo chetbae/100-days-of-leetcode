2827febabcf85c046a3efcd44ae8cfb7b4d2d5a5
chetbae <48145854+chetbae@users.noreply.github.com>
2024-04-17 17:27:16 -0400

    add 198 dp solution O(1) space

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -10,3 +10,14 @@ class Solution:
             dp[i] = max(dp[i-2] + nums[i-1], dp[i-1])
         
         return dp[-1]
+
+# space optimized
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev1, prev2 = 0, nums[0] # dp[0] -> dp[i-2], dp[1] -> dp[i-1]
+
+        # index 0, 1 are prev1, prev2. calculate the rest of the dynamic prog array
+        for i in range(2, len(nums)+1):
+            prev1, prev2 = prev2, max(prev1 + nums[i-1], prev2)
+        
+        return prev2
