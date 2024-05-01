1b13d0f3c108e36d7c94d5ff1b17880edccfc093
chetbae <48145854+chetbae@users.noreply.github.com>
2024-05-01 16:22:07 -0400

    add 19 1-pass solution

--- 19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list.py
@@ -29,3 +29,35 @@ class Solution:
         prev.next = node.next.next if node.next else node.next
 
         return head
+
+
+# one-pass solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        end = node = head
+        prev = None
+
+        # send end to explore, then when n steps in, increment node and prev to find target
+        # ()->(prev)->(node)->...n-2 steps...->(end)->...
+
+        # run for n times
+        for i in range(n):
+            end = end.next
+        
+        # increment all pointers until end
+        while end:
+            prev, node, end = node, node.next, end.next
+
+        # if target is the head node, return the next node i.e. cutting head out
+        if node == head:
+            return head.next
+
+        # otherwise cut out target node and return
+        prev.next = node.next
+        return head
