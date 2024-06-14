24b07544379b865de20fbd35e423624157662b87
chetbae <48145854+chetbae@users.noreply.github.com>
2024-06-14 13:24:32 -0400

    add 23 div-and-cqr solution

--- 23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists.py
@@ -25,3 +25,34 @@ class Solution:
                 lists[k] = node.next
         
         return dummy.next
+
+## divide and conquer approach
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        
+        mid = len(lists) // 2
+        left, right = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(left, right)
+    
+    def merge(self, left, right):
+        if not left:
+            return right
+        if not right: 
+            return left
+        
+        if left.val <= right.val:
+            left.next = self.merge(left.next, right)
+            return left
+        else:
+            right.next = self.merge(left, right.next)
+            return right
